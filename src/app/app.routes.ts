071a6e1b2dbdb9cec3c1e8f03b9ba4985168909e
foagick <hamedfuad@gmail.com>
2026-07-27 12:57:32 +0300

    feat: add course detail component and enhance student dashboard with course catalog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: "dashboard",
-        loadComponent: () => import("./features/student-dashboard/student-dashboard.component")
-        .then((m) => m.StudentDashboardComponent),
-    },
-    { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/student-dashboard/student-dashboard.component').then(
+        (m) => m.StudentDashboardComponent,
+      ),
+  },
+  {
+    path: 'courses/:id',
+    loadComponent: () =>
+      import('./features/course-detail/course-detail.component').then(
+        (m) => m.CourseDetailComponent,
+      ),
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 ];
