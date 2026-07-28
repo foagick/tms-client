6c07fa270ea4395635f0ec6691a2af259ce35edd
foagick <hamedfuad@gmail.com>
2026-07-28 19:15:08 +0300

    course detail page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,10 @@ export const routes: Routes = [
         loadComponent: () => import("./features/student-dashboard/student-dashboard.component")
         .then((m) => m.StudentDashboardComponent),
     },
+    {
+        path: "courses/:id",
+        loadComponent: () => import("./features/course-detail/course-detail")
+        .then((m) => m.CourseDetail),
+    },
     { path: "", redirectTo: "dashboard", pathMatch: "full" },
 ];
