a0474b2f07326cfc3746e1867d64a02977bed491
foagick <hamedfuad@gmail.com>
2026-07-29 01:44:42 +0300

    Api integration

--- src/app/features/student-dashboard/student-dashboard.component.ts
+++ src/app/features/student-dashboard/student-dashboard.component.ts
@@ -1,6 +1,8 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, inject } from '@angular/core';
 import { CourseCard } from '../../ui/course-card/course-card';
 import { Course } from '../../models/course.model';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-student-dashboard',
@@ -10,6 +12,8 @@ import { Course } from '../../models/course.model';
   styleUrl: './student-dashboard.component.scss',
 })
 export class StudentDashboardComponent {
+  private api = inject(CourseService);
+
   studentName = signal("Liya Kebede");
   earnedCredits = signal(45);
 
@@ -18,36 +22,9 @@ export class StudentDashboardComponent {
   
    selectedCourse = signal<Course | null>(null);
 
-  availableCourses = signal<Course[]>([
-    {
-      id: 1,
-      title: 'Advanced Java Services',
-      code: 'CSE-101',
-      maxCapacity: 30,
-      enrollmentCount: 10,
-    },
-    {
-      id: 2,
-      title: 'Angular UI Lab',
-      code: 'CSE-210',
-      maxCapacity: 25,
-      enrollmentCount: 25,
-    },
-    {
-      id: 3,
-      title: 'Database Design',
-      code: 'CSE-305',
-      maxCapacity: 20,
-      enrollmentCount: 18,
-    },
-    {
-      id: 4,
-      title: 'API Security Workshop',
-      code: 'CSE-420',
-      maxCapacity: 40,
-      enrollmentCount: 15,
-    },
-  ]);
+  coursesResource = rxResource({
+  stream: () => this.api.getAll(),
+  });
 
   registerForClass() {
     this.earnedCredits.update((c) => c + 3);
