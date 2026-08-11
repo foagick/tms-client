2c24fc7e3c770a0300a53b73ce34c59e802f6881
foagick <hamedfuad@gmail.com>
2026-08-11 18:14:10 +0300

    auth added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from "@angular/common/http";
 
 import { routes } from './app.routes';
+import { credentialsInterceptor } from './interceptors/credentials.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([credentialsInterceptor]),
+      withXsrfConfiguration({
+      cookieName: 'XSRF-TOKEN', // Cookie name set by .NET server
+      headerName: 'X-XSRF-TOKEN', // Header expected by .NET server
+      })
+    ),
   ]
 };
