d509be162caf7aa81ce2dfd406dc9e39403896fc
ephrem-011 <ephraimfekadu@gmail.com>
2026-08-24 18:56:15 +0300

    feat: enhance routing and add JWT interceptor for improved security

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,14 @@ import { provideHttpClient, withInterceptors, withXsrfConfiguration } from "@ang
 import { routes } from './app.routes';
 import { credentialsInterceptor } from './interceptors/credentials.interceptor';
 import { errorInterceptor } from './interceptors/error.interceptor';
+import { jwtInterceptor } from './interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(
-      withInterceptors([credentialsInterceptor,  errorInterceptor]),
+      withInterceptors([credentialsInterceptor,  errorInterceptor, jwtInterceptor]),
       withXsrfConfiguration({
       cookieName: 'XSRF-TOKEN', // Cookie name set by .NET server
       headerName: 'X-XSRF-TOKEN', // Header expected by .NET server
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,42 +1,55 @@
 import { Routes } from '@angular/router';
 import { roleGuard } from './guards/role.guard';
 import { AdminCourseListComponent } from './features/admin-course-list-component/admin-course-list-component';
+import { Login } from './features/login/login';
 
 export const routes: Routes = [
-    {
-        path: "login",
-        loadComponent: () => import("./features/login/login")
-        .then((m) => m.Login),
-    },{
-        path: "dashboard",
-        loadComponent: () => import("./features/student-dashboard/student-dashboard.component")
-        .then((m) => m.StudentDashboardComponent),
-    },{
-        path: "instructor-dashboard",
-        loadComponent: () => import("./features/instructor-dashboard/instructor-dashboard")
-        .then((m) => m.InstructorDashboard),
-    },
-    {
-        path: "courses/:id",
-        loadComponent: () => import("./features/course-detail/course-detail")
-        .then((m) => m.CourseDetail),
-    },{
-        path: 'enroll',
-        loadComponent: () => import('./features/enrollment-form/enrollment-form')
-        .then(m => m.EnrollmentForm),
-    },{
-        path: 'enrollments',
-        loadComponent: () => import('./features/enrollment-list/enrollment-list.component')
-        .then(m => m.EnrollmentListComponent),
-    },{
-        path: 'grade-submission',
-        loadComponent: () =>
-            import('./features/grade-submission/grade-submission.component')
-            .then(m => m.GradeSubmissionComponent)
-    },{
-        path: 'admin/courses',
-        component: AdminCourseListComponent,
-        canActivate: [roleGuard('Admin')]
-    },
-    { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/student-dashboard/student-dashboard.component').then(
+        (m) => m.StudentDashboardComponent,
+      ),
+  },
+  {
+    path: 'instructor-dashboard',
+    loadComponent: () =>
+      import('./features/instructor-dashboard/instructor-dashboard').then(
+        (m) => m.InstructorDashboard,
+      ),
+  },
+  {
+    path: 'courses/:id',
+    loadComponent: () =>
+      import('./features/course-detail/course-detail').then((m) => m.CourseDetail),
+  },
+  {
+    path: 'enroll',
+    loadComponent: () =>
+      import('./features/enrollment-form/enrollment-form').then((m) => m.EnrollmentForm),
+  },
+  {
+    path: 'enrollments',
+    loadComponent: () =>
+      import('./features/enrollment-list/enrollment-list.component').then(
+        (m) => m.EnrollmentListComponent,
+      ),
+  },
+  {
+    path: 'grade-submission',
+    loadComponent: () =>
+      import('./features/grade-submission/grade-submission.component').then(
+        (m) => m.GradeSubmissionComponent,
+      ),
+  },
+  {
+    path: 'admin/courses',
+    component: AdminCourseListComponent,
+    canActivate: [roleGuard('Admin')],
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 ];
